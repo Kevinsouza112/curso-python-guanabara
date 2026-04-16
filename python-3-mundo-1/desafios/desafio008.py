a8d131861bc06a9f50d95947d7bf737d3c986441
Kevin Souza <kevinsouza112@gmail.com>
2026-04-15 22:22:31 -0300

    Corrigir desafios e renomear nomenclatura
    - Remover código incompleto/duplicado em desafio 007
    - Remover comentário órfão em desafio 015
    - Renomear desafios de 001-015 para 01-15 (formato mais limpo)

--- python-3-mundo-1/desafios/desafio008.py
+++
@@ -1,4 +0,0 @@
-print("====DESAFIO 088====")
-n=float(input("Digite um numero em metros: "))
-
-print('{} metros é igual a {} centímetros e igual a {} milímetros'.format(n,n*100,n*1000))
---
+++ python-3-mundo-1/desafios/desafio08.py
@@ -0,0 +1,6 @@
+print("====DESAFIO 088====")
+n=float(input("Digite um numero em metros: "))
+
+print('{} metros é igual a {} centímetros e igual a {} milímetros'.format(n,n*100,n*1000))
+
+
