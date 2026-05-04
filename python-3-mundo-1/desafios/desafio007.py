f587adfb89492032dd2a135c4cc00edabfd5b700
Kevinsouza112 <kevinsouza112@gmail.com>
2026-05-04 15:19:14 -0300

    atualiza desafios 007 e 015

--- python-3-mundo-1/desafios/desafio007.py
+++
@@ -1,7 +0,0 @@
-print('===== DESAFIO 007 =====')
-nt1=float(input('Digite uma nota: '))
-nt2=float(input('Digite uma nota: '))
-media=(nt1+nt2)/2
-
-print('A média das notas é {:.1f}'.format(media))
-
